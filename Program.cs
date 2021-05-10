cbc71768a8dd7483155e09a2a96660750e650941
Mashaal Ahmed <mashaalahmed@refugeetalent.com>
2021-05-10 11:59:28 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DuckPractice_DesignPatterns
+{
+    public abstract class Duck
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
